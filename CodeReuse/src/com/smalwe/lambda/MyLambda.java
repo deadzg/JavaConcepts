d7a2a06653f675a79441c2dbbbf7a5cd2f622098
deadzg <sunny.malwe6@gmail.com>
2020-03-25 16:23:52 +0000

    Method references

--- CodeReuse/src/com/smalwe/lambda/MyLambda.java
+++ CodeReuse/src/com/smalwe/lambda/MyLambda.java
@@ -3,10 +3,14 @@ package com.smalwe.lambda;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.concurrent.Callable;
+import java.util.function.BiPredicate;
+import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class MyLambda {
@@ -65,6 +69,30 @@ public class MyLambda {
         // Read two lines
         System.out.println(processFile((BufferedReader br) ->  br.readLine() + br.readLine()));
 
+        Predicate<String> nonEmptyStringPred = (s) -> !s.isEmpty();
+
+        List<String> nonEmpty = filter(Arrays.asList("adf", "", "ddddd"), nonEmptyStringPred);
+        System.out.println(nonEmpty.size());
+
+        Consumer<Integer> printAllIntegers = (i) -> System.out.print(i);
+        myForEach(Arrays.asList(1,2,3), printAllIntegers);
+
+        Function<String, Integer> stringToLenFun = (String s) -> s.length();
+        System.out.println(myMap(Arrays.asList("dddd", "ffff", "dd"), stringToLenFun));
+
+        List<String> slist = Arrays.asList("abc", "wdf", "aee");
+
+        slist.sort((s1,s2)-> s1.compareToIgnoreCase(s2));
+        // or
+        slist.sort(String::compareToIgnoreCase);
+
+        Function<String, Integer> stringToInteger = (String s) -> Integer.parseInt(s);
+        // or
+        Function<String, Integer> stringToIntegerNew = Integer::parseInt;
+
+        BiPredicate<List<String>, String> contains = (list, element) -> list.contains(element);
+        // or
+        BiPredicate<List<String>, String> containsNew = List::contains;
     }
 
     public static void process(Runnable r) {
@@ -82,5 +110,29 @@ public class MyLambda {
     public static String processFile(BufferredReaderProcessor p) throws IOException {
         BufferedReader br = new BufferedReader(new FileReader("mydata.txt"));
         return p.process(br);
+    }
+
+    public static <T> List<T> filter (List<T> list, Predicate<T> p) {
+        List<T> results = new ArrayList<>();
+        for(T t: list) {
+            if(p.test(t)){
+                results.add(t);
+            }
+        }
+        return results;
+    }
+
+    public static <T> void myForEach(List<T> list, Consumer<T> c) {
+        for(T t : list) {
+            c.accept(t);
+        }
+    }
+
+    public static <T,R> List<R> myMap(List<T> list, Function<T,R> f) {
+        List<R> rs = new ArrayList<>();
+        for(T t: list) {
+            rs.add(f.apply(t));
+        }
+        return rs;
     }
 }
